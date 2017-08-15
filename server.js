91d545d39bb6c8380368166138c8a6fe0b0f8c79
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-15 14:12:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get ('/article-one',function (req, res) {
 });
 
 app.get('/article-two',function (req, res){
-    res.send('Article two requested and will be served here');
+    res.sendfile(path.join(--dirname, 'ui', 'Article Two.html'));
 });
 
 app.get('/article-three',function (req, res) {
