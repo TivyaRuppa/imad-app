db4e27cb8c5fa1cace1f63f13a81197805f8b08e
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-16 15:20:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,63 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    title: 'Article One | Tivya',
+    heading: 'Article One',
+    date: '12,August 2017',
+    content:`<div>
+                Hi All!!!
+            </div>
+            <div>
+                <p>
+                  This is my first article in IMAD. I'm very much excited by learning these things. Still I can't believe that I'm doing this by myself!!!  
+                </p>
+                <p>
+                    I'm now happy by joining this course and eagerly awaiting to learn more & more stuffs.
+                </p>'`
+}
+function createtemplate (data){
+    var title = data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+var htmltemplate = `
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div>
+                <a href='/'>Home</a>
+         </div>
+            <div class="Tivya">
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            </div>
+            <hr/>
+            <div class = "container">
+            ${content}
+            </div>
+            </div>
+    </body>
+</html>'`;
+return htmltemplate
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get ('/article-one',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'Article One.html'));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function (req, res){
