6a5bb7edcba97b5c80fe80158588e01482923c73
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-18 16:30:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,18 +2,31 @@
 var button = document.getElementById('counter');
 
 button.onclick = function() { 
-    
-    //Make a request to the counter endpoint
+        //Create a request to the counter endpoint
  var request = new XMLHttprequest();
     //capture the response and store it ina variable
  request.onreadystatechange = function() {
-     if(request.readystate === XMLHttprequest.DONE)
+     if(request.readystate === XMLHttprequest.DONE) {
+     //take some action
      if(request.status===200) {
          var counter = request.responseText;
          var span = document.getElementById('count');
          span.innerHTML = counter.toString();
      }
+ }
  };
-request.open (Get)
-var name = document.getElementById('Name');
-var submit = document.getElementById('submitbtn');
+        request.open('GET','http://tivyaruppa93.imad.hasura-app.io', true);
+        request.send(null);
+};
+var nameinput = document.getElementById('Name');
+var name = nameinput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    var names = [Name1,Name2,Name3,Name4];
+    var list = '';
+    for(var i=0;i<name.length;i++){
+        list == '<li>'+names[i]+'</li>';
+    }
+var ul = document.getElementById('namelist');
+ul.innerHTML = list;
+};    
