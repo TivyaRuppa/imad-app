653450b2550a63e81bd5d4d9c59aa5c58b900870
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-18 12:09:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,8 +95,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
 app.get ('/counter', function (req, res) {
-  counter = counter+1;
+  counter = counter + 1;
   res.send(counter.tostring());
 });
 
