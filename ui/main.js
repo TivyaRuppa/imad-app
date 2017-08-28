8b66264287d1710f985c18781616a8cc1d75c0b6
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-28 19:30:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,15 @@
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function(){
-   counter = counter+1;
-   var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
+    var request = new XMLHttprequest();
+    request.onresdystatechange = function (){
+      if(request.readystate === XMLHttprequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();
+          }
+      } 
+    };
+  request.open('GET', 'http://tivyaruppa93.imad.hasura-app.io/counter', true);  
+  request.send(null);
 };
