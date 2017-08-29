f21ba483e03a85eeca250e922a548e8c91d91606
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-29 20:24:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,15 @@ function exec(){
     request.send(); 
     console.log('EXECUTED');
 }
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(i=0; i<name.length;i++){
+     list == '<li>' + names[i] + '</li>';  
+    }
+var ul = document.getElementById('namelist');
+ul.innerHTML=list;
+};
