96b606a9799aeba94024fcb4e81196db31af944f
TivyaRuppa <30837157+TivyaRuppa@users.noreply.github.com>
2017-08-29 20:04:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,19 @@
-var button = document.getElementById('counter');
-button.onclick = function(){
+function exec(){
     var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-        if(request.staus === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }
-        }
-};
-    request.open('GET','http://tivyaruppa93.imad.hasura-app.io/counter',true);
-    request.send();
-};   
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
+    request.open("GET", "http://kogam22.imad.hasura-app.io/counter", true);
+    request.send(); 
+    console.log('EXECUTED');
+}
